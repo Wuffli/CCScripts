6ec83c775cc7e6a840b7c3441c59d54ceb090359
Luna Lorea Zehnder <lunazehn@gmail.com>
2023-08-02 21:54:38 +0200

    Update mobfarm.lua

--- mobfarm.lua
+++ mobfarm.lua
@@ -1,46 +1,75 @@
-import using local buttons = require("buttons")
- 
-local Buttons = require("Buttons")
+local Buttons = require("buttons")
 local monitor = peripheral.find("monitor")
- 
-redstone.setOutput("back", true)
-redstone.setOutput("right", false)
-redstone.setOutput("left", false)
- 
-function Light()
-    redstone.setOutput("back", not redstone.getOutput("back"))
-end
- 
-function Grinder()
-    redstone.setOutput("left", not redstone.getOutput("left"))
-end
- 
-function Fans()
-    redstone.setOutput("right", not redstone.getOutput("right"))
-end
- 
- 
- 
-local buttons = {
-                { name = "Light", func = Light },
-                { name = "Grinder", func = Grinder },
-                { name = "Fans", func = Fans }
-                }
- 
-buttonDisplay = ButtonDisplay:new()
- 
-for index, button in ipairs(buttons) do
-    buttonDisplay:newButton(button.name)
-end
- 
+
+local buttonDisplay = ButtonDisplay:new()
 buttonDisplay.monitor = monitor
+
+local buttonsList = {
+    { name = "Global Button", globalButton = true },
+    { name = "Lightswitch", outputDirection = "back", offState = false },
+    { name = "Mob Grinders", outputDirection = "left", offState = false  },
+    { name = "Fans", outputDirection = "right", offState = false  }
+    }
+
+buttonDisplay:buttonsFromList(buttonsList)
 buttonDisplay:render()
+
+function switchOutput(direction)
+    local currentOutput = not redstone.getOutput(direction)
+    redstone.setOutput( direction, currentOutput )
+end
+
+function setAll(state)
+    for index, button in ipairs(buttonsList) do
+        if button.outputDirection ~= nil then
+
+            -- output = not xor(state, button.offState)
+            redstone.setOutput( button.outputDirection, state )
+
+        end
+        buttonDisplay.buttons[index].isPressed = state
+    end
+end
+
+function globalButton()
+    setAll(buttonDisplay.buttons[1].isPressed)
+end
+
+function checkIfAllAreOffOrOn()
+    b = buttonDisplay.buttons
+    if not ( b[2].isPressed and b[3].isPressed and b[4].isPressed ) then 
+        b[1].isPressed = false
+    end
+    if ( b[2].isPressed and b[3].isPressed and b[4].isPressed ) then 
+        b[1].isPressed = true
+    end
+end
+
+function buttonPressed(button)
+    if button.globalButton then
+        globalButton()
+
+    elseif button.outputDirection ~= nil then
+        switchOutput(button.outputDirection)
+        checkIfAllAreOffOrOn()
+    end
+
+    
+end
+
+
+setAll(false)
  
 while true do
     local index = buttonDisplay:waitForButtonPress()
-    buttons[index].func()
+
+    if index ~= -1 then
+        buttonPressed(buttonsList[index])
     
-    buttonDisplay:render()
+        buttonDisplay:render()
+    end
 end
+ 
+ 
  
  
